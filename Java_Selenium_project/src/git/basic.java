3f88fdfef69fa926ec257f58fdb137b125dbc720
quali <quali@host.docker.internal>
2025-07-19 19:15:55 +0400

    Jen_Test

---
+++ Java_Selenium_project/src/git/basic.java
@@ -0,0 +1,14 @@
+package git;
+
+public class basic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	    System.out.println("Hello World");
+        System.out.println("Starplatinum");
+		
+		
+	}
+
+}
